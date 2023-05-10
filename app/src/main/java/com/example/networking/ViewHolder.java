b7215211b569e6d319e68a176ea277cf12e39239
a22robko <a22robko@student.his.se>
2023-05-10 13:07:21 +0200

    The code for completing task 5 is not fully functional yet, as the values ex "kinnekulla" are being sent but not displayed.

---
+++ app/src/main/java/com/example/networking/ViewHolder.java
@@ -0,0 +1,20 @@
+/*package com.example.networking;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    public TextView name;
+    public TextView size;
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        name = itemView.findViewById(R.id.name);
+    }
+}*/
+
+
